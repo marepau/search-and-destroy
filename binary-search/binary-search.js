d76f12dbc76c3cb48cf16437c3021989969dc905
Mary Paul <marepau@gmail.com>
2019-12-11 15:43:34 -0500

    binary search passing

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,9 +2,32 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
-
+	const mid = Math.ceil((array.length)/2)
+	const midVal = array[mid]
+	if(midVal === target){
+		return true
+	}
+	if (target <= midVal){
+		const smallArray = array.slice(0, mid)
+		while(smallArray.length > 1){
+		return binarySearch(smallArray, target)
+		}
+		if(smallArray[0] === target){
+			return true
+		}
+		else return false
+	}
+	else {
+		const bigArray = array.slice(mid+1, array.length)
+		while(bigArray.length > 1){
+			return binarySearch(bigArray, target)
+		}
+		if(bigArray[0] === target){
+			return true
+		}
+		else return false
+	}
+}
 /*
 	EXTRA CREDIT:
 
